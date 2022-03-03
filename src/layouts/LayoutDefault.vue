626ed559da4789a3058bb83bd2b6fae6cb5d9e72
Jackson Aquino Primo <jackson.primodev@gmail.com>
2022-03-03 18:40:35 -0300

    adjusted LayoutDefault

--- src/layouts/LayoutDefault.vue
+++ src/layouts/LayoutDefault.vue
@@ -1,7 +1,29 @@
 <script lang="ts">
+import { authStore } from "../stores/auth";
+import axios from "../services/axiosService";
+import { storeToRefs } from "pinia";
 export default {
+  setup() {
+    const store = authStore();
+    const { authenticated, userAuthenticated, tokens } = storeToRefs(store);
+    const { signOut } = store;
+    return {
+      authenticated,
+      userAuthenticated,
+      tokens,
+      signOut,
+    };
+  },
   name: "LayoutDefault",
-  data: () => ({ tab: "images" }),
+  data: () => ({
+    tab: "Home",
+  }),
+  methods: {
+    logout() {
+      axios.delete(`auth/signout/${this.tokens.refreshToken}`);
+      this.signOut();
+    },
+  },
 };
 </script>
 <template>
@@ -12,9 +34,31 @@ export default {
       </q-toolbar>
 
       <q-tabs v-model="tab">
-        <q-tab name="Home" label="Home" icon="home" />
-        <q-tab name="Login" label="Login" icon="person" />
+        <q-route-tab name="Home" label="Home" icon="home" to="/" exact />
         <q-tab name="Top 100" label="Top 100" icon="whatshot" />
+        <q-route-tab
+          v-if="!authenticated"
+          name="Login"
+          label="Login"
+          icon="person"
+          to="/login"
+          exact
+        />
+
+        <q-btn-dropdown
+          v-if="authenticated"
+          auto-close
+          stretch
+          flat
+          icon="person"
+          :label="userAuthenticated.email"
+        >
+          <q-list>
+            <q-item clickable @click="logout">
+              <q-item-section>Logout</q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
       </q-tabs>
     </q-header>
 
