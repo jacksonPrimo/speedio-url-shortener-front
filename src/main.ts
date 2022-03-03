c3f89185414987c7eb1d0f0ae7ad498c2ad61693
Jackson Aquino Primo <jackson.primodev@gmail.com>
2022-03-03 18:36:27 -0300

    added sweetAlert config

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import { Quasar } from "quasar";
+import VueSweetalert2 from "vue-sweetalert2";
 
+import "sweetalert2/dist/sweetalert2.min.css";
 import "@quasar/extras/material-icons/material-icons.css";
 import "quasar/src/css/index.sass";
 
@@ -14,6 +16,7 @@ app.use(Quasar, {
   plugins: {},
 });
 
+app.use(VueSweetalert2);
 app.use(createPinia());
 app.use(router);
 
