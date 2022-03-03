79f87781239176b5d66efd1c746b7d1736101ba9
Jackson Aquino Primo <jackson.primodev@gmail.com>
2022-03-03 10:39:28 -0300

    added quasar config in project

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView } from "vue-router";
+import LayoutDefault from "./layouts/LayoutDefault.vue";
 </script>
 
 <template>
-  <RouterView />
+  <LayoutDefault>
+    <RouterView />
+  </LayoutDefault>
 </template>
 
 <style>
-@import '@/assets/base.css';
+@import "@/assets/base.css";
 
 #app {
   max-width: 1280px;
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
+import { Quasar } from "quasar";
 
-const app = createApp(App)
+import "@quasar/extras/material-icons/material-icons.css";
+import "quasar/src/css/index.sass";
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(Quasar, {
+  plugins: {},
+});
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
