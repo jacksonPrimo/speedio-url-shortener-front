2a9f424071cabfe552de6a197c0035bb88cd6df3
Jackson Aquino Primo <jackson.primodev@gmail.com>
2022-03-03 10:17:55 -0300

    added project initial template

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+export default {
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<template>
+  <h1 class="green">{{ msg }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+</style>
